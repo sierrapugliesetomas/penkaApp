f972bac760ccb77f1e0d9dd0b4decd6de7141196
Tomas <T.sierra@redlink.com.ar>
2021-04-07 00:50:17 -0300

    refactor para generar codigo penka

--- src/app/core/services/code-penka.service.ts
+++ src/app/core/services/code-penka.service.ts
@@ -5,11 +5,16 @@ import {Injectable} from '@angular/core';
 })
 export class CodePenkaService {
     codePenka = '';
-    constructor() {
+
+    constructor() { }
+
+    generateCodePenka(): string {
+        this.codePenka = '';
         const characters = 'AbCdEfGhIjKvWxYz0123456789';
         const charactersLength = characters.length;
         for (let i = 0; i < charactersLength; i++) {
             this.codePenka += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
+        return this.codePenka;
     }
 }
