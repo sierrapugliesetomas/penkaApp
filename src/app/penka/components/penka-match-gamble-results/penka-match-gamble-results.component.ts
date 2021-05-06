eaf0a8554914015e12c035c3807807974807560c
Tomas <T.sierra@redlink.com.ar>
2021-05-06 16:54:12 -0300

    label campeonato/liga/copa

--- src/app/penka/components/penka-match-gamble-results/penka-match-gamble-results.component.ts
+++ src/app/penka/components/penka-match-gamble-results/penka-match-gamble-results.component.ts
@@ -31,6 +31,7 @@ export class PenkaMatchGambleResultsComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        debugger;
         this.user = this.firebase.auth().currentUser;
         this.singleMatchService.getMatchById(this.match.singleMatchId).subscribe(
             res => this.singleMatch = res,
--- src/app/penka/components/penka-match-gamble/penka-match-gamble.component.ts
+++ src/app/penka/components/penka-match-gamble/penka-match-gamble.component.ts
@@ -4,6 +4,9 @@ import {User} from '../../../core/interfaces/user';
 import {FirebaseApp} from '@angular/fire';
 import {AuthService} from '../../../core/services/auth.service';
 import {GambleService} from '../../../core/services/gamble.service';
+import { SingleMatch } from 'src/app/core/interfaces/single-match';
+import { SingleMatchesService } from 'src/app/core/services/single-matches.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-penka-match-gamble',
@@ -16,6 +19,7 @@ export class PenkaMatchGambleComponent implements OnInit {
 
   @Input() match: Gamble;
   save: string;
+  singleMatch: SingleMatch;
 
   newGamble = {} as Gamble;
   user = {} as User;
@@ -23,7 +27,7 @@ export class PenkaMatchGambleComponent implements OnInit {
   constructor(
     public firebase: FirebaseApp,
     public auth: AuthService,
-    private gambleService: GambleService) {
+    private singleMatchService: SingleMatchesService) {
   }
 
   ngOnInit(): void {
@@ -31,6 +35,12 @@ export class PenkaMatchGambleComponent implements OnInit {
     if (this.match.penkaFormat === 'MEDIUM') {
       this.setDefaultRadioButtonValue();
     }
+
+    this.singleMatchService.getMatchById(this.match.singleMatchId)
+    .pipe(take(1))
+    .subscribe(res => {
+      this.singleMatch = res;
+    })
   }
 
   private setDefaultRadioButtonValue(): void {
