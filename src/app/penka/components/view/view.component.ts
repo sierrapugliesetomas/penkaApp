3ea9ded89991a399cba4bdb5bb12ba2a316a0c04
Tomas <T.sierra@redlink.com.ar>
2021-05-24 22:30:48 -0300

    ver mas penka pop

--- src/app/penka/components/view/view.component.ts
+++ src/app/penka/components/view/view.component.ts
@@ -1,65 +1,85 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, Params, Router} from '@angular/router';
-import {PenkasService} from '../../../core/services/penkas.service';
-import {Subject} from 'rxjs';
-import {FirebaseApp} from '@angular/fire';
-import {AuthService} from '../../../core/services/auth.service';
-import {User} from '../../../core/interfaces/user';
-import {takeUntil} from 'rxjs/operators';
-import {ListMatchesService} from '../../../core/services/list-matches.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { PenkasService } from '../../../core/services/penkas.service';
+import { Subject } from 'rxjs';
+import { FirebaseApp } from '@angular/fire';
+import { AuthService } from '../../../core/services/auth.service';
+import { User } from '../../../core/interfaces/user';
+import { takeUntil } from 'rxjs/operators';
+import { ListMatchesService } from '../../../core/services/list-matches.service';
+import { Penka } from 'src/app/core/interfaces/penka';
 
 @Component({
-    selector: 'app-view',
-    templateUrl: './view.component.html',
-    styleUrls: ['./view.component.scss']
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.scss'],
 })
 export class ViewComponent implements OnInit, OnDestroy {
-    penkaId: string;
-    penka;
-    user = {} as User;
-    listMatches = [];
-    private unsubscribe$ = new Subject<void>();
+  penkaId: string;
+  penka: Penka;
+  user = {} as User;
+  listMatches = [];
+  private unsubscribe$ = new Subject<void>();
 
-    constructor(
-        public firebase: FirebaseApp,
-        public auth: AuthService,
-        private router: Router,
-        private activatedRoute: ActivatedRoute,
-        private penkasService: PenkasService,
-        private listMatchesService: ListMatchesService) {
+  constructor(
+    public firebase: FirebaseApp,
+    public auth: AuthService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private penkasService: PenkasService,
+    private listMatchesService: ListMatchesService
+  ) {}
+
+  ngOnInit(): void {
+    this.getUser();
+    this.getCodePenka();
+    this.getPenka();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  private getUser(): void {
+    this.user = this.firebase.auth().currentUser;
+  }
+
+  private getCodePenka(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.penkaId = params.codePenka;
+    });
+  }
+
+  private getPenka(): void {
+    this.penkasService
+    .getPenkaByCodePenka(this.penkaId)
+    .pipe(takeUntil(this.unsubscribe$))
+    .subscribe((res) => {
+      this.penka = res[0];
+      this.getListMatches();
+    });
+  }
+
+  private getListMatches(): void {
+    if(this.penka.codeTemplate !== '') {
+      this.listMatchesService
+      .getListMatchesByCodeTemplate(this.penka.codeTemplate)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((res) => {
+        this.listMatches = res;
+      });
+    } else {
+      this.listMatchesService
+      .getListMatchesByCodePenka(this.penka.codePenka)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((res) => {
+        this.listMatches = res;
+      });
     }
+  }
 
-    ngOnInit(): void {
-        this.user = this.firebase.auth().currentUser;
-        this.activatedRoute.params.subscribe(
-            (params: Params) => {
-                this.penkaId = params.id;
-            }
-        );
-        this.penkasService.getPenkaById(this.penkaId)
-            .pipe(
-                takeUntil(this.unsubscribe$)
-            )
-            .subscribe(
-                res => {
-                    this.penka = res;
-                });
-        this.listMatchesService.getListMatches()
-            .pipe(
-                takeUntil(this.unsubscribe$)
-            )
-            .subscribe(
-                res => {
-                    this.listMatches = res;
-                });
-    }
 
-    ngOnDestroy(): void {
-        this.unsubscribe$.next();
-        this.unsubscribe$.complete();
-    }
 
-    join(): void {
-
-    }
+  join(): void {}
 }
